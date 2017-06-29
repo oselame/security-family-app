b914b2e82e2e7acd5ec9860c8db1c99f9532f84e
Adriano Oselame <adriano.oselame@softplan.com.br>
2017-06-29 11:02:55 -0300

    inclusao getLocationsByNameDate

--- back-end/controllers/location-controller.js
+++ back-end/controllers/location-controller.js
@@ -12,7 +12,7 @@ class LocationController {
         this.router.get('/', this.getLocations.bind(this));
         this.router.post('/', this.postLocation.bind(this));
         this.router.get('/:name', this.getLocationsByName.bind(this));
-       // this.router.put('/:id', this.putPlayer.bind(this));
+        this.router.get('/:name/:date', this.getLocationsByNameDate.bind(this));
     }
 
     getLocations(req, res) {
@@ -40,49 +40,17 @@ class LocationController {
         }
     }
 
-
-
-/*getSinglePlayer(req, res) {
-        var id = req.params.id;
-        var player = PlayersService.getSinglePlayer(id);
-        if (!player) {
+    getLocationsByNameDate(req, res) {
+        var name = req.params.name;
+        var date = req.params.date;
+        var locations = LocationService.getLocationsByNameDate(name, date);
+        if (!locations) {
             res.sendStatus(404);
         } else {
-            res.send(player);
+            res.send(locations);
         }
     }
 
-    putPlayer(req, res) {
-        var id = parseInt(req.params.id, 10);
-        var existingPlayer = PlayersService.getSinglePlayer(id);
-        if (!existingPlayer) {
-            var playerInfo = req.body;
-            playerInfo.id = id;
-            if (PlayersService.addPlayer(playerInfo)) {
-                res.setHeader('Location', '/players/' + id);
-                res.sendStatus(201);
-            } else {
-                res.sendStatus(500);
-            }
-        } else {
-            if (PlayersService.updatePlayer(id, req.body)) {
-                res.sendStatus(204);
-            } else {
-                res.sendStatus(404);
-            }
-        }
-    }
-
-    postPlayer(req, res) {
-        var playerInfo = req.body;
-        if (PlayersService.addPlayer(playerInfo)) {
-            res.setHeader('Location', '/players/' + playerInfo.id);
-            res.sendStatus(200);
-        } else {
-            res.sendStatus(500);
-        }
-    }
-    */
 }
 
 module.exports = LocationController;
--- back-end/services/location-service.js
+++ back-end/services/location-service.js
@@ -7,6 +7,21 @@ class LocationService {
         this.locations = [];
     }
 
+    getDate() {
+        var data = new Date();
+        var ano = data.getFullYear();
+        var mes = data.getMonth() + 1;
+        var dia = data.getDay();
+        mes = mes < 10 ? "0" + mes : mes;
+        dia = dia < 10 ? "0" + dia : dia;
+        return parseInt("" + ano + mes + dia);
+    }
+
+    getTime() {
+        var data = new Date();
+        return data.getTime();
+    }
+
     getLocations() {
         return this.locations;
     }
@@ -17,12 +32,13 @@ class LocationService {
                     && loc.latitude === location.latitude)).length > 0) {
             return false;
         }
-        location.id = uuid.v4();
-        location.datetime = new Date();
-
+        //location.id = uuid.v4();
+        location.date = this.getDate();
+        location.time = this.getTime();
+        location.name = location.name.toUpperCase();
+        location.latitude = parseFloat(location.latitude);
+        location.longitude = parseFloat(location.longitude);
         this.locations.push(location);
-
-        console.log(this.locations);
         return true;
     }
 
@@ -30,41 +46,17 @@ class LocationService {
         if (!name) {
             return false;
         }
-        var lcs = this.locations.filter( loc => (loc.name === name));
+        var lcs = this.locations.filter( loc => (loc.name === name.toUpperCase()));
         return lcs || null;
     }
 
-/*
-    getSinglePlayer(playerId) {
-        var player = this.players.filter(p => p.id === playerId)[0];
-
-        return player || null;
-    }
-
-    addPlayer(info) {
-        // prevent a bit of bad/duplicate data
-        if (!info || this.players.filter(p => (p.firstName === info.firstName && p.lastName === info.lastName)).length > 0) {
+    getLocationsByNameDate(name, date) {
+        if (!name || !date) {
             return false;
         }
-
-        info.id = uuid.v4();
-
-        this.players.push(info);
-        return true;
+        var lcs = this.locations.filter( loc => (loc.name === name.toUpperCase() && loc.date === date));
+        return lcs || null;
     }
-
-    updatePlayer(playerId, info) {
-        var player = this.getSinglePlayer(playerId);
-        if (player) {
-            player.firstName = info.firstName ? info.firstName : player.firstName;
-            player.lastName = info.lastName ? info.lastName : player.lastName;
-            player.displayName = info.displayName ? info.displayName : player.displayName;
-
-            return true;
-        }
-        return false;
-    }
-    */
 }
 
 module.exports = new LocationService();
